c44eaa837dd18fd602d462bd919c1a500ea131f6
LENOVO G410 <iimrobani@gmail.com>
2017-04-15 17:39:36 +0700

    Pertama upload

---
+++ app/src/main/java/com/example/lenovog410/uplodgit/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lenovog410.uplodgit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void test(){
+        int i = 1;
+    }
+}
